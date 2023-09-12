2df18439edc14f12f5005d59ea8e9de87b24c326
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-09-12 13:00:32 -0300

    feat(section-2): created section 2

---
+++ src/app/components/PortfolioSection.tsx
@@ -0,0 +1,11 @@
+export const PortfolioSection = () => {
+  return (
+    <section className="bg-zinc-950 w-full">
+      <div className="max-w-6xl w-full m-auto px-4 py-24 flex gap-8 flex-col justify-center items-center">
+        <h2 className="text-gray-200 text-5xl font-bold max-w-2xl text-center">
+          por-t-fó-lio
+        </h2>
+      </div>
+    </section>
+  );
+};
