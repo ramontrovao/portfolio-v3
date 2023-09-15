882a78238ef486ec2b1dd32ccf1e11d2fd7e5141
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-09-14 22:55:08 -0300

    feat(add-see-more): added pagination to experience and education section

--- src/app/components/ExperienceAndEducation.tsx
+++ src/app/components/ExperienceAndEducation.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import { Card } from "fragments/Card";
+import { useArrayPagination } from "hooks/useArrayPagination";
 import { TEducation, TExperience } from "types/THygraphData";
 
 interface IExperienceAndEducationProps {
@@ -10,15 +13,22 @@ export const ExperienceAndEducation = ({
   experiences,
   educations,
 }: IExperienceAndEducationProps) => {
+  const experiencesWithPagination = useArrayPagination(1, experiences);
+  const educationsWithPagination = useArrayPagination(1, educations);
+
   const sliceFullDate = (date: string) => {
     const slicedDate = date.slice(0, 7);
 
     return slicedDate;
   };
 
+  const handleNextExperiencesPage = () => experiencesWithPagination.nextPage();
+
+  const handleNextEducationsPage = () => educationsWithPagination.nextPage();
+
   return (
     <section className="bg-zinc-950 w-full">
-      <div className="max-w-6xl w-full m-auto p-4 flex gap-16 justify-center md:justify-between items-center flex-wrap md:flex-nowrap">
+      <div className="max-w-6xl w-full m-auto p-4 flex gap-16 justify-center md:justify-between flex-wrap md:flex-nowrap">
         <article className="w-full md:w-1/2">
           <header>
             <h2 className="text-gray-200 text-5xl font-bold max-w-2xl text-center">
@@ -27,20 +37,28 @@ export const ExperienceAndEducation = ({
           </header>
 
           <main className="w-full mt-8 flex flex-col justify-center items-center gap-4">
-            {experiences.map((experience) => (
-              <Card
-                cardName={experience.name}
-                rightText={`${sliceFullDate(experience.startDate)} ~ ${
-                  experience.endDate
-                    ? sliceFullDate(experience.endDate)
-                    : "atual"
-                }`}
-                rightTextSize="md"
-                cardDescription={experience.shortDescription}
-                key={experience.startDate}
-              />
-            ))}
+            {(experiencesWithPagination.arrayUpdated as TExperience[]).map(
+              (experience) => (
+                <Card
+                  cardName={experience.name}
+                  rightText={`${sliceFullDate(experience.startDate)} ~ ${
+                    experience.endDate
+                      ? sliceFullDate(experience.endDate)
+                      : "atual"
+                  }`}
+                  rightTextSize="md"
+                  cardDescription={experience.shortDescription}
+                  key={experience.startDate}
+                />
+              )
+            )}
           </main>
+
+          <button
+            onClick={handleNextExperiencesPage}
+            className="mt-4 bg-none border-none text-lg font-normal text-gray-400 transition-all duration-300 hover:opacity-80">
+            ver mais
+          </button>
         </article>
 
         <article className="w-full md:w-1/2">
@@ -51,18 +69,28 @@ export const ExperienceAndEducation = ({
           </header>
 
           <main className="w-full mt-8 flex flex-col justify-center items-center gap-4">
-            {educations.map((education) => (
-              <Card
-                cardName={education.name}
-                rightText={`${sliceFullDate(education.startDate)} ~ ${
-                  education.endDate ? sliceFullDate(education.endDate) : "atual"
-                }`}
-                rightTextSize="md"
-                cardDescription={education.shortDescription}
-                key={education.startDate}
-              />
-            ))}
+            {(educationsWithPagination.arrayUpdated as TEducation[]).map(
+              (education) => (
+                <Card
+                  cardName={education.name}
+                  rightText={`${sliceFullDate(education.startDate)} ~ ${
+                    education.endDate
+                      ? sliceFullDate(education.endDate)
+                      : "atual"
+                  }`}
+                  rightTextSize="md"
+                  cardDescription={education.shortDescription}
+                  key={education.startDate}
+                />
+              )
+            )}
           </main>
+
+          <button
+            onClick={handleNextEducationsPage}
+            className="mt-4 bg-none border-none text-lg font-normal text-gray-400 transition-all duration-300 hover:opacity-80">
+            ver mais
+          </button>
         </article>
       </div>
     </section>
