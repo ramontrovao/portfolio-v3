8a62ab48397ad2ffbf26a419422a32f23721cd97
Ramon Pinheiro <ramonzinhg12@gmail.com>
2023-09-16 12:11:04 -0300

    feat(projects): finished projects page

--- src/fragments/Project.tsx
+++ src/fragments/Project.tsx
@@ -9,7 +9,7 @@ export const Project = ({
   project: { name, description, projectTechnologies, wallpaper },
 }: IProjectProps) => {
   return (
-    <article className="w-full pl-4 py-4 max-w-[45rem] min-h-[15rem] flex gap-6 flex-wrap justify-center items-center overflow-hidden bg-gradient-to-r from-zinc-950 to-zinc-900 border border-gray-500 rounded-md md:flex-nowrap md:justify-between">
+    <article className="w-full max-md:pr-4 pl-4 py-4 max-w-[45rem] min-h-[15rem] flex gap-6 flex-wrap justify-center items-center overflow-hidden bg-gradient-to-r from-zinc-950 to-zinc-900 border border-gray-500 rounded-md md:flex-nowrap md:justify-between">
       <div className="flex flex-col gap-6">
         <h2 className="text-gray-200 font-bold text-2xl">{name}</h2>
 
@@ -25,15 +25,13 @@ export const Project = ({
         </ul>
       </div>
 
-      <div className="w-[75%]">
-        <Image
-          className="rounded-md -mr-5 transition-all duration-300 hover:-mr-0 max-w-[20rem] min-h-[15rem]"
-          src={wallpaper.url}
-          width={400}
-          height={400}
-          alt=""
-        />
-      </div>
+      <Image
+        className="rounded-md w-full md:-mr-5 transition-all duration-300 md:hover:-mr-0 md:max-w-[20rem] min-h-[15rem]"
+        src={wallpaper.url}
+        width={400}
+        height={400}
+        alt=""
+      />
     </article>
   );
 };
