98c7ad9c6bc5167af5260e90b3fd1f800cdc8f55
ramon trovão <ramonzinhg12@gmail.com>
2023-09-30 09:47:29 -0300

    feat: created links page

---
+++ src/app/links/components/LinksSection.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export const LinksSection = () => {
+  return (
+    <section
+      id="sobre-mim"
+      className="bg-zinc-950 w-full">
+      <div className="max-w-6xl h-screen w-full m-auto px-4 py-16 flex gap-16 flex-col justify-center items-center">
+        <Image
+          src="https://github.com/ramontrovao.png"
+          alt=""
+          width={350}
+          height={350}
+          className="rounded-full max-w-[12.5rem]"
+        />
+      </div>
+    </section>
+  );
+};
---
+++ src/app/links/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import { LinksSection } from "./components/LinksSection";
+
+export default function Links() {
+  return (
+    <main>
+      <LinksSection />
+    </main>
+  );
+}
