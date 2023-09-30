19803fe2b34f3afe5f9710ea68fbe46b136f8cfd
ramon trovão <ramonzinhg12@gmail.com>
2023-09-30 10:22:44 -0300

    feat: integrated with hygraph cms

--- src/app/links/components/LinksSection.tsx
+++ src/app/links/components/LinksSection.tsx
@@ -1,14 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
 
-import logo from "../../../assets/logo.svg";
+import logo from "assets/logo.svg";
+import type { TSocialMedia } from "types/THygraphData";
 
-export const LinksSection = () => {
+interface ILinksSectionProps {
+  links: TSocialMedia[];
+}
+
+export const LinksSection = ({ links }: ILinksSectionProps) => {
   return (
     <section
       id="sobre-mim"
       className="bg-zinc-950 w-full">
-      <div className="max-w-6xl h-screen w-full m-auto px-4 py-16 flex gap-16 flex-col justify-center items-center">
+      <div className="max-w-6xl h-screen w-full m-auto px-4 py-32 flex gap-16 flex-col justify-center items-center">
         <Image
           src="https://github.com/ramontrovao.png"
           alt=""
@@ -17,15 +22,34 @@ export const LinksSection = () => {
           className="rounded-full max-w-[12.5rem]"
         />
 
-        <ul className="w-full max-w-sm">
-          <li className="w-full flex gap-4 justify-center items-center border border-gray-400 rounded-lg text-gray-200 transition-all duration-300 hover:opacity-80 text-lg font-normal p-4">
-            <Image
-              className="w-5"
-              src={logo}
-              alt=""
-            />
-            <Link href="/">portfólio</Link>
+        <ul className="w-full max-w-sm flex flex-col gap-8">
+          <li className="w-full border border-gray-400 rounded-lg text-gray-200 transition-all duration-300 hover:opacity-80 text-lg font-normal p-4">
+            <Link
+              href="/"
+              className="flex gap-4 justify-center items-center">
+              <Image
+                className="w-5"
+                src={logo}
+                width={350}
+                height={350}
+                alt=""
+              />
+              <span>portfólio</span>
+            </Link>
           </li>
+
+          {links.map((link) => (
+            <li
+              key={link.id}
+              className="w-full border border-gray-400 rounded-lg text-gray-200 transition-all duration-300 hover:opacity-80 text-lg font-normal p-4">
+              <a
+                className="flex gap-4 justify-center items-center"
+                href={link.url}>
+                <span dangerouslySetInnerHTML={{ __html: link.logoSvg }} />
+                {link.name}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
     </section>
--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,10 +1,30 @@
 import Image from "next/image";
 import { LinksSection } from "./components/LinksSection";
+import { fetchHygraphQuery } from "utils/fetchHygraphQuery";
 
-export default function Links() {
+const getLinksData = async () => {
+  const query = `query LinksQuery {
+        page(where: {slug: "links"}) {
+          info {
+            socialMedias {
+              id
+              name
+              logoSvg
+              url
+            }
+          }
+        }
+      }`;
+
+  return fetchHygraphQuery(query);
+};
+
+export default async function Links() {
+  const { page } = await getLinksData();
+
   return (
     <main>
-      <LinksSection />
+      <LinksSection links={page.info.socialMedias} />
     </main>
   );
 }
