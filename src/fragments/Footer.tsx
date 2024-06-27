4cb86c38a712e1beb73277496dc94203cb5b9a87
ramon trovão <ramonzinhg12@gmail.com>
2024-06-26 22:48:41 -0300

    feat(footer): add i18n to footer

--- src/fragments/Footer.tsx
+++ src/fragments/Footer.tsx
@@ -2,21 +2,20 @@ import Image from "next/image";
 
 import logo from "assets/logo.svg";
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 export const Footer = () => {
+  const t = useTranslations("footer");
+
   return (
     <footer className="w-full flex bg-zinc-950">
       <div className="max-w-6xl w-full border-t border-zinc-900 px-4 py-16 m-auto flex justify-between items-center max-sm:gap-4 max-sm:flex-wrap-reverse">
         <div className="text-gray-200">
-          <p>feito com muito &hearts; por ramon trovão</p>
-          <span>
-            &copy; Todos os direitos reservados - Ramon Pinheiro Trovão da Silva
-          </span>
+          <p>{t("made_with_love")}</p>
+          <span>{t("legal_rights")}</span>
         </div>
 
-        <Link
-          href="#"
-          className="transition-all duration-300 hover:opacity-80">
+        <Link href="#" className="transition-all duration-300 hover:opacity-80">
           <Image
             className="max-w-[2rem]"
             src={logo}
