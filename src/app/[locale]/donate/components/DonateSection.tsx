407c41aeb40dd353ff7f951e944166bbe4b93cde
ramon trovão <ramonzinhg12@gmail.com>
2024-06-30 23:04:32 -0300

    feat(projects): add english translation to donate page

--- src/app/[locale]/donate/components/DonateSection.tsx
+++ src/app/[locale]/donate/components/DonateSection.tsx
@@ -1,31 +1,34 @@
-import Image from "next/image"
+import { useTranslations } from "next-intl";
+import Image from "next/image";
 
 export const DonateSection = () => {
-    return (
-        <section
-        id="contato"
-        className="bg-zinc-950 w-full">
-        <div className="max-w-6xl w-full min-h-screen-without-footer-and-header  m-auto px-4 py-32 flex gap-16 flex-col justify-center items-center">
-          <h2 className="text-gray-400 text-xl md:text-3xl font-normal max-w-2xl text-center">
-            me pague um café ou uma skin através do pix abaixo =D
-          </h2>
+  const t = useTranslations("donate");
 
-          <Image
-            src="https://github.com/ramontrovao.png"
-            alt=""
-            width={350}
-            height={350}
-            className="rounded-full max-w-[12.5rem]"
-          />
-  
-          <div className="flex flex-col gap-4">
-            <strong className="text-gray-200 transition-all duration-300 hover:opacity-80 text-2xl md:text-4xl font-bold max-w-2xl text-center">
-                pix@ramontrovao.dev
-            </strong>
+  return (
+    <section id="contato" className="bg-zinc-950 w-full">
+      <div className="max-w-6xl w-full min-h-screen-without-footer-and-header  m-auto px-4 py-32 flex gap-16 flex-col justify-center items-center">
+        <h2 className="text-gray-400 text-xl md:text-3xl font-normal max-w-2xl text-center">
+          {t("donate_title")}
+        </h2>
 
-            <span className="text-gray-400 text-md font-normal text-center">em nome de Ramon Pinheiro Trovao da Silva</span>
-          </div>
+        <Image
+          src="https://github.com/ramontrovao.png"
+          alt=""
+          width={350}
+          height={350}
+          className="rounded-full max-w-[12.5rem]"
+        />
+
+        <div className="flex flex-col gap-4">
+          <strong className="text-gray-200 transition-all duration-300 hover:opacity-80 text-2xl md:text-4xl font-bold max-w-2xl text-center">
+            pix@ramontrovao.dev
+          </strong>
+
+          <span className="text-gray-400 text-md font-normal text-center">
+            {t("donate_description")}
+          </span>
         </div>
-      </section>
-    )
-}
+      </div>
+    </section>
+  );
+};
