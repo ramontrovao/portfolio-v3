e2f1f57e80a4d6e604d3a245b32650dd531a3d2a
ramon trovão <ramonzinhg12@gmail.com>
2024-06-30 23:01:25 -0300

    feat(projects): add english translation to links page

--- src/app/[locale]/links/components/LinksSection.tsx
+++ src/app/[locale]/links/components/LinksSection.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 import logo from "assets/logo.svg";
 import type { TSocialMedia } from "types/THygraphData";
+import { useTranslations } from "next-intl";
 
 interface ILinksSectionProps {
   links: TSocialMedia[];
@@ -10,10 +11,10 @@ interface ILinksSectionProps {
 }
 
 export const LinksSection = ({ links, name }: ILinksSectionProps) => {
+  const t = useTranslations("links");
+
   return (
-    <section
-      id="sobre-mim"
-      className="bg-zinc-950 w-full">
+    <section id="sobre-mim" className="bg-zinc-950 w-full">
       <div className="max-w-6xl h-screen w-full m-auto px-4 py-32 flex gap-12 flex-col justify-center items-center">
         <div className="flex flex-col justify-center items-center gap-4">
           <Image
@@ -25,7 +26,7 @@ export const LinksSection = ({ links, name }: ILinksSectionProps) => {
           />
 
           <strong className="text-2xl text-gray-200 font-normal">
-            {name} - meus links :)
+            {t("links_title", { name })}
           </strong>
         </div>
 
@@ -33,7 +34,8 @@ export const LinksSection = ({ links, name }: ILinksSectionProps) => {
           <li className="w-full border border-gray-400 rounded-lg text-gray-200 transition-all duration-300 hover:opacity-80 text-lg font-normal">
             <Link
               href="/"
-              className="p-4 flex gap-4 justify-center items-center">
+              className="p-4 flex gap-4 justify-center items-center"
+            >
               <Image
                 className="w-5"
                 src={logo}
@@ -41,18 +43,20 @@ export const LinksSection = ({ links, name }: ILinksSectionProps) => {
                 height={350}
                 alt=""
               />
-              <span>portfólio</span>
+              <span>{t("portfolio")}</span>
             </Link>
           </li>
 
           {links.map((link) => (
             <li
               key={link.id}
-              className="w-full border border-gray-400 rounded-lg text-gray-200 transition-all duration-300 hover:opacity-80 text-lg font-normal">
+              className="w-full border border-gray-400 rounded-lg text-gray-200 transition-all duration-300 hover:opacity-80 text-lg font-normal"
+            >
               <a
                 className="p-4 flex gap-4 justify-center items-center"
                 href={link.url}
-                target="_blank">
+                target="_blank"
+              >
                 <span dangerouslySetInnerHTML={{ __html: link.logoSvg }} />
                 {link.name}
               </a>
